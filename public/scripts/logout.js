4c37a5d5788fe834af9dca5d3b795eb25cef396a
Pablo Perez <pabloperezzz16@gmail.com>
2026-03-28 20:02:36 -0300

    Implemented logout functionality.
    Finished the nav bar and added it to all pages.
    
    Need to start working on styles.

--- public/scripts/logout.js
+++ public/scripts/logout.js
@@ -2,7 +2,9 @@ import { customFetch } from "./aux.js";
 
 const logoutBtn = document.getElementById("logoutBtn");
 
-logoutBtn.addEventListener("click", async (e) => {
-  e.preventDefault();
-  customFetch("/logout", "POST", "/");
-});
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    customFetch("/logout", "POST", "/");
+  });
+}
